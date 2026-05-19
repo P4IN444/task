544186e7c926f87e5c112bcc6eab09df04fa864d
Aboubaker Amajjoud <aamajjou@e1r10p2.1337.ma>
2026-05-19 18:16:57 +0100

    add CD deployment job

--- main.c
+++ main.c
@@ -1,29 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aamajjou <aamajjou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 18:10:06 by aamajjou          #+#    #+#             */
+/*   Updated: 2026/05/19 18:14:52 by aamajjou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int	main(void)
 {
-    printf("Running valid tests...\n");
-    
-    // 1. Valid Test: ft_strlen
-    assert(ft_strlen("Hello") == 5);
-    
-    // 2. Valid Test: ft_toupper
-    assert(ft_toupper('a') == 'A');
-    
-    // 3. Valid Test: ft_strjoin
-    char *str = ft_strjoin("42", "Network");
-    assert(strcmp(str, "42Network") == 0);
-    free(str);
-    
-    printf("Valid tests passed!\n");
+	char	*str;
 
-    // 4. Intentional Failing Test
-    printf("Running the failing test...\n");
-    assert(ft_strlen("Fail") == 99); // This is false and will cause an abort
-
-    return (0);
+	printf("Running valid tests...\n");
+	assert(ft_strlen("Hello") == 5);
+	assert(ft_toupper('a') == 'A');
+	str = ft_strjoin("42", "Network");
+	assert(strcmp(str, "42Network") == 0);
+	free(str);
+	
+	printf("Valid tests passed!\n");
+	printf("fix and Running the failing test...\n");
+	assert(ft_strlen("pass") == 4);
+	return (0);
 }
